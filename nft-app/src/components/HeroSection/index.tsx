eb73d680c8dbd6b87be7ddb80784b67891e2bbc2
Tanner <40257656+TAKEYE01@users.noreply.github.com>
2022-05-19 18:14:05 -0400

    Set up components/pages Added Navbar, Herosection, Footer to components Imported/Installed styled components

---
+++ nft-app/src/components/HeroSection/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div>Hero Section Component</div>
+  )
+}
+
+export default HeroSection;
---
+++ nft-app/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import Footer from '../components/Footer';
+
+const Landing = () => {
+  return (
+    <>
+        <Navbar />
+        <HeroSection />
+        <Footer />
+    </>
+  )
+}
+
+export default Landing;
